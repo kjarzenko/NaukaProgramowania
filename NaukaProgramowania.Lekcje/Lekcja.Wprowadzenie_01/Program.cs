d9008230c33eb78e882574e184d55754912837ff
kjarzenko <k.jarzenko@gmail.com>
2021-10-13 18:56:57 +0200

    Zmiana nr 1 - dodanie projektu do repo

---
+++ NaukaProgramowania.Lekcje/Lekcja.Wprowadzenie_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lekcja.Wprowadzenie_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
